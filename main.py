a4aadfe8005c10f3b8ae8e4158c65814d509e9b2
chibibaku <chibibaku@outlook.jp>
2021-09-04 21:44:04 +0900

    Improved log output

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import time
 
 x = input()
 x = int(x)
+count = 0
 
 while x!=1:
     if x%2==1:
         x=x*3+1
     else:
         x=x/2
-    print(x)
+    count += 1
+    r = str(count).zfill(4)
+    print("[Step : ",r,"][x :",'{:.0f}'.format(x).rjust(10),"]")
     time.sleep(0.05)
+    
